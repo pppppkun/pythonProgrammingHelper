2c1d70d43798b2af0880b64222ae01115fed9d16
pkun <936637810@qq.com>
2020-07-05 16:39:19 +0800

    feat: 完成面向用例检测的80%

--- CaseOriented/utils.py
+++ CaseOriented/utils.py
@@ -1,7 +1,8 @@
 import os
 import shutil
 import zipfile
-import time
+import re
+import json
 
 
 def unzip(zip_name):
@@ -25,7 +26,7 @@ def rm_file(dont_rm_file):
             os.remove(_)
 
 
-def detect_use_case_oriented(file_path, case_path):
+def detect_use_case_oriented(file_path, case_path, id):
     """
     :param file_path: the file which students upload
     :param case_path: the use-case which will be input
@@ -35,69 +36,82 @@ def detect_use_case_oriented(file_path, case_path):
     f = open(file_path, encoding='utf-8')
     s = eval(c.read())
     main_ = f.read()
+    prints = get_print(main_)
     count = 0
-    threshold = 0.15
+    threshold = 0
     for case in s:
-        in_ = case['input']
+        # in_ = case['input']
         out_ = case['output']
-        if in_ in main_ or out_ in main_:
-            count += 1
+        outs = out_.split('\n')
+        for _ in outs:
+            if _ == '':
+                continue
+            for __ in prints:
+                if _ in __:
+                    count += 1
+                    print(_+" "+__)
+                    break
     if count/len(s) > threshold:
-        print('this upload is user-case oriented')
-    else:
-        print('pass')
-    return count/len(s)
+        print('The {} upload is user-case oriented'.format(id), end=' ')
+        print('Num of cases which match in this code : {}:{} ({}%)'.format(count, len(s), count/len(s)*100))
+        return 1
+    return 0
 
 
-def cal_time_complex(file_path, case_path):
-    # TODO
-    c = open(case_path, encoding='utf-8')
-    f = open(file_path, encoding='utf-8')
-    s = eval(c.read())
-    for case in s:
-        in_ = case['input']
-        test = open('testCases.in', 'w')
-        test.write(in_)
-        test.close()
-        code = 'main.py'
-        start = time.time()
-        os.system('python ' + code + ' < ' + 'testCases.in' + ' >> ' + 'testCases.in')
-        end = time.time()
-        print(end - start)
+def get_print(main_function):
+    pattern = re.compile('.*(print\(.*\)).*')
+    p = pattern.findall(main_function)
+    return p
 
 
+def cd():
+    s = os.getcwd()
+    s = ''.join(reversed(s))
+    for _ in range(len(os.getcwd())):
+        if s[_] == '\\':
+            break
+    os.chdir(os.getcwd()[:len(s)-_-1])
+
+
+def get_score():
+    f = open('sample.json', encoding='utf-8')
+    res = f.read()
+    data = json.loads(res)
+    return data
 
 if __name__ == '__main__':
-    MAIN = 'main.py'
-    CASE = '.mooctest\\testCases.json'
-    file = os.getcwd() + '\\train'
-    os.chdir(file)
-    fstack = list()
-    for _ in os.listdir(os.getcwd()):
-        fstack.append(os.getcwd())
-        os.chdir(os.getcwd()+"\\"+_)
-        # print(os.getcwd())
-        for __ in os.listdir(os.getcwd()):
-            fstack.append(os.getcwd())
-            os.chdir(os.getcwd()+"\\"+__)
-            # print(os.getcwd())
-            for ___ in os.listdir(os.getcwd()):
-                if not os.path.isdir(___):
-                    continue
-                fstack.append(os.getcwd())
-                os.chdir(os.getcwd()+"\\"+___)
-                # print(os.getcwd())
-                for filename in os.listdir(os.getcwd()):
-                    print(os.getcwd())
-                    unzip(filename)
-                    detect_use_case_oriented(MAIN, CASE)
-                    rm_file(filename)
-                os.chdir(fstack[-1])
-                fstack.pop(-1)
-            os.chdir(fstack[-1])
-            fstack.pop(-1)
-        os.chdir(fstack[-1])
-        fstack.pop(-1)
+    cd()
+    get_score()
+#     MAIN = 'main.py'
+#     CASE = '.mooctest\\testCases.json'
+#     file = os.getcwd() + '\\train'
+#     os.chdir(file)
+#     fstack = list()
+#     for _ in os.listdir(os.getcwd()):
+#         fstack.append(os.getcwd())
+#         os.chdir(os.getcwd()+"\\"+_)
+#         # print(os.getcwd())
+#         for __ in os.listdir(os.getcwd()):
+#             fstack.append(os.getcwd())
+#             os.chdir(os.getcwd()+"\\"+__)
+#             # print(os.getcwd())
+#             for ___ in os.listdir(os.getcwd()):
+#                 if not os.path.isdir(___):
+#                     continue
+#                 fstack.append(os.getcwd())
+#                 os.chdir(os.getcwd()+"\\"+___)
+#                 # print(os.getcwd())
+#                 for filename in os.listdir(os.getcwd()):
+#                     print(os.getcwd())
+#                     unzip(filename)
+#                     detect_use_case_oriented(MAIN, CASE)
+#                     rm_file(filename)
+#                 os.chdir(fstack[-1])
+#                 fstack.pop(-1)
+#             os.chdir(fstack[-1])
+#             fstack.pop(-1)
+#         os.chdir(fstack[-1])
+#         fstack.pop(-1)
 
 
 
