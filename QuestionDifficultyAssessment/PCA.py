fc79d7707fad27a24c302134132d34e653844df7
何文兵 <181250043@smail.nju.edu.cn>
2020-07-11 21:50:44 +0800

    doc: meeting result

--- QuestionDifficultyAssessment/PCA.py
+++ QuestionDifficultyAssessment/PCA.py
@@ -1,10 +1,16 @@
 import numpy as np
 from sklearn.decomposition import PCA
+from matplotlib import pyplot as plt
 
-X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
-pca = PCA(n_components=2, copy=True, whiten=False)
+X = np.array([[-1, -1, -5], [-2, -1, 5], [-3, -2, -5], [1, 1, -5], [2, 1, 5], [3, 2, 5]])
+pca = PCA(n_components=3, copy=True, whiten=False)
 newX = pca.fit_transform(X)
 print(X)
 print(newX)
 print(pca.explained_variance_ratio_)
+print(pca.score_samples(X))
+for i in range(6):
+    plt.scatter(newX[i, 0], newX[i, 1], alpha=0.8)
+plt.show()
 
+#[0.77747542 0.2192404 ]
