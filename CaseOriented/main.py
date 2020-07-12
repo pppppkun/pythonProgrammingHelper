babdc8ed6840948859b5fcfa248ff8970033bb47
pkun <936637810@qq.com>
2020-07-12 14:47:26 +0800

    feat: add csv and result

--- CaseOriented/main.py
+++ CaseOriented/main.py
@@ -1,7 +1,9 @@
 import os
 import utils
+import zipfile
 
 # init
+origin = os.getcwd()
 utils.cd()
 MAIN = 'main.py'
 CASE = '.mooctest\\testCases.json'
@@ -28,12 +30,17 @@ if __name__ == '__main__':
                 os.chdir(os.getcwd() + "\\" + ___)
                 # print(os.getcwd())
                 for filename in os.listdir(os.getcwd()):
-                    utils.unzip(filename)
-                    res = utils.detect_use_case_oriented(MAIN, CASE, _, __, ___, result)
-                    if res != -1:
-                        noc += 1
-                    total += 1
-                    utils.rm_file(filename)
+                    try:
+                        utils.unzip(filename)
+                        res = utils.detect_use_case_oriented(MAIN, CASE, _, __, ___, result)
+                        if res != -1:
+                            noc += 1
+                        total += 1
+                        utils.rm_file(filename)
+                    except PermissionError:
+                        print(_, __, ___)
+                    except zipfile.BadZipFile:
+                        print(_, __, ___)
                 os.chdir(fstack[-1])
                 fstack.pop(-1)
             os.chdir(fstack[-1])
@@ -42,28 +49,32 @@ if __name__ == '__main__':
         fstack.pop(-1)
     print(noc, total, noc / total)
     sorted(result, key=lambda x: result[x].sort_key)
-    for _ in result:
-        print(result[_])
-    while True:
-        print("请输入userId和caseId来定位具体的提交")
-        userId = input()
-        caseId = input()
-        print(result[userId+' '+caseId])
-        print('有嫌疑的提交为')
-        for _ in result[userId + ' ' + caseId].match_records:
-            print(_, result[userId+' '+caseId].match_records[_])
-        print("是否查看具体代码，需要请输出1，否则输入0")
-        f = int(input())
-        while True:
-            if f == 1:
-                print("请输入提交id，如果想查看其他提交请输出0")
-                uploadId = input()
-                if uploadId == '0':
-                    break
-                result[userId + ' ' + caseId].get_code(uploadId)
-            elif f == 0:
-                break
-            else:
-                print("请重新输入")
-                f = int(input())
+    # for _ in result:
+    #     print(result[_])
+    os.chdir(origin)
+    utils.get_msg(result)
+
+    # while True:
+    #     print("请输入userId和caseId来定位具体的提交")
+    #     userId = input()
+    #     caseId = input()
+    #     print(result[userId+' '+caseId])
+    #     print('有嫌疑的提交为')
+    #     for _ in result[userId + ' ' + caseId].match_records:
+    #         print(_, result[userId+' '+caseId].match_records[_])
+    #     print("是否查看具体代码，需要请输出1，否则输入0")
+    #     f = int(input())
+    #     while True:
+    #         if f == 1:
+    #             print("请输入提交id，如果想查看其他提交请输出0")
+    #             uploadId = input()
+    #             if uploadId == '0':
+    #                 break
+    #             result[userId + ' ' + caseId].get_code(uploadId)
+    #         elif f == 0:
+    #             break
+    #         else:
+    #             print("请重新输入")
+    #             f = int(input())
+
 
--- CaseOriented/utils.py
+++ CaseOriented/utils.py
@@ -3,6 +3,7 @@ import shutil
 import zipfile
 import re
 import json
+import csv
 from Case import Case
 
 def unzip(zip_name):
@@ -49,7 +50,7 @@ def detect_use_case_oriented(file_path, case_path, user_id, case_id, upload_id, 
         in_ = case['input']
         ins = in_.split('\n')
         for _ in ins:
-            if _ == '':
+            if _ == '' or len(_) == 1:
                 continue
             for __ in lines:
                 if _ in __:
@@ -110,17 +111,86 @@ def get_score(user_id, case_id):
             return Case(_, user_id)
 
 
+def get_msg(res):
+    _ = open('result.log', encoding='utf-8')
+    cor = eval(_.read())
+    _.close()
+    cd()
+    f = open('sample.json', encoding='utf-8')
+    _ = f.read()
+    data = json.loads(_)
+    r = dict()
+
+    for _ in data:
+        for __ in data[_]['cases']:
+            case_id = __['case_id']
+            if case_id not in r:
+                r[case_id] = dict()
+                r[case_id]['users'] = 0  # 提交人数
+                r[case_id]['uploads'] = 0  # 提交次数
+                r[case_id]['average'] = 0  # 总得分
+                r[case_id]['100'] = 0  # 满分比例
+            r[case_id]['users'] += 1
+            if _ + ' ' + case_id in res:
+                s = 0
+                for ___ in __['upload_records']:
+                    if str(___['upload_id']) not in res[_+' '+case_id].match_records:
+                        r[case_id]['uploads'] += 1
+                        s = max(s, ___['score'])
+                r[case_id]['average'] += s
+                if s == 100:
+                    r[case_id]['100'] += 1
+            else:
+                r[case_id]['uploads'] += len(__['upload_records'])
+                r[case_id]['average'] += __['final_score']
+                if __['final_score'] == 100:
+                    r[case_id]['100'] += 1
+    for _ in r:
+        r[_]['100'] = r[_]['100']/r[_]['users']
+        try:
+            r[_]['average'] = r[_]['average'] / r[_]['uploads']
+        except ZeroDivisionError:
+            print(_)
+            print(r[_])
+    l = dict()
+    for _ in res:
+        c = _.split(' ')[1]
+        if c in l:
+            l[c] += 1
+        else:
+            l[c] = 1
+    for _ in r:
+        if _ in l:
+            r[_]['users'] = l[_]/r[_]['users']
+        else:
+            r[_]['users'] = 0
+    csvfile = open('csv_test.csv', 'w', newline='')
+    writer = csv.writer(csvfile, delimiter='\n')
+    writer.writerow([','.join(['case_id', 'case_oriented_ratio', 'final_average_score', 'full_score_ratio', 'upload_times'])])
+    for _ in r:
+        s = []
+        s.append(_)
+        s.append(str(r[_]['users']))
+        s.append(str(r[_]['average']))
+        s.append(str(r[_]['100']))
+        s.append(str(r[_]['uploads']))
+        writer.writerow([','.join(s)])
+    csvfile.close()
+
+
 
 if __name__ == '__main__':
-    # code = r'E:\PythonProject\bigHomework\train\3544\2172线性表\268885\main.py'
-    # case = r'E:\PythonProject\bigHomework\train\3544\2172线性表\268885\.mooctest\testCases.json'
-    # raa = dict()
-    # c = detect_use_case_oriented(code, case, user_id='3544', case_id='2172线性表', upload_id='268885', r=raa)
-    # print(raa['3544 2172'])
-    # print(raa['3544 2172'].match_records['268885'])
+    code = r'E:\PythonProject\bigHomework\train\3544\2172线性表\268885\main.py'
+    case = r'E:\PythonProject\bigHomework\train\3544\2172线性表\268885\.mooctest\testCases.json'
+    raa = dict()
+    c = detect_use_case_oriented(code, case, user_id='3544', case_id='2172线性表', upload_id='268885', r=raa)
+    print(raa['3544 2172'])
+    print(raa['3544 2172'].match_records['268885'])
     a = {'a': 2, 'b': 1, 'c': 3}
     print(a)
     print(sorted(a))
     print(sorted(a, key=lambda x:a[x]))
+
+
 
 
