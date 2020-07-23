71b303fb6a4112e075309d334274be19fa73dd4b
何文兵 <181250043@smail.nju.edu.cn>
2020-07-23 23:37:19 +0800

    feat: k-means demo

--- QuestionDifficultyAssessment/PCA.py
+++ QuestionDifficultyAssessment/PCA.py
@@ -1,7 +1,9 @@
 import numpy as np
 from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
 from matplotlib import pyplot as plt
 import csv
+import pandas as pd
 
 file3 = csv.reader(open('final.csv', 'r'))
 rows = list(file3)
@@ -9,13 +11,19 @@ rows.remove(rows[0])
 for i in range(len(rows)):
     rows[i] = rows[i][1:]
 X = np.array(rows)
-pca = PCA(n_components=3, copy=True, whiten=True)
+pca = PCA(n_components=2, svd_solver='full', copy=True, whiten=True)
 newX = pca.fit_transform(X)
-print(newX)
-print(pca.explained_variance_ratio_)
-#print(pca.score_samples(X))
-#for i in range(6):
-    #plt.scatter(newX[i, 0], newX[i, 1], alpha=0.8)
-#plt.show()
 
-#[0.77747542 0.2192404 ]
+for i in range(len(newX)):
+    plt.scatter(newX[i][0], newX[i][1])
+plt.show()
+
+# K-means cluster analysis
+
+kmeans = KMeans(n_clusters=8, copy_x=True).fit_predict(newX)
+
+
+data = pd.read_csv("final.csv")
+data["classfication"] = kmeans
+data.to_csv("final.csv", mode='w', index=False)
+
