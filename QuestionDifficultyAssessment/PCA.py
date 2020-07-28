e130d11b52f5ad63ec0ee545ea9c106196325fb8
何文兵 <181250043@smail.nju.edu.cn>
2020-07-28 20:28:12 +0800

    fix: improved PCA and KMeans (data standardization)

--- QuestionDifficultyAssessment/PCA.py
+++ QuestionDifficultyAssessment/PCA.py
@@ -1,7 +1,9 @@
 import numpy as np
 from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
 from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import csv
 import pandas as pd
 import KmeansAssessment
@@ -11,31 +13,35 @@ rows = list(file3)
 rows.remove(rows[0])
 for i in range(len(rows)):
     rows[i] = rows[i][1:]
-X = np.array(rows)
-pca = PCA(n_components=2, svd_solver='full', copy=True, whiten=True)
-newX = pca.fit_transform(X)
+x = np.array(rows)
+scaler = StandardScaler()
+X_train = scaler.fit_transform(x,)
+pca = PCA(n_components=3, svd_solver='full', copy=True, whiten=True)
+newX = pca.fit_transform(X_train)
 print(pca.explained_variance_ratio_)
 
 # K-means cluster analysis
-k = KmeansAssessment.Assessment(newX)
-k.assessment()
+#k = KmeansAssessment.Assessment(newX)
+#k.assessment()
 
-kmeans = KMeans(n_clusters=6, copy_x=True).fit(newX)
+kmeans = KMeans(n_clusters=4, copy_x=True).fit(newX)
 print(kmeans.cluster_centers_)
 print(kmeans.inertia_)
+fig = plt.figure()
+ax = Axes3D(fig)
 for i in range(len(newX)):
     if kmeans.labels_[i] == 0:
-        plt.scatter(newX[i][0], newX[i][1], c="blue")
+        ax.scatter(newX[i][0], newX[i][1], newX[i][2], c="blue")
     elif kmeans.labels_[i] == 1:
-        plt.scatter(newX[i][0], newX[i][1], c="red")
+        ax.scatter(newX[i][0], newX[i][1], newX[i][2], c="red")
     elif kmeans.labels_[i] == 2:
-        plt.scatter(newX[i][0], newX[i][1], c="yellow")
+        ax.scatter(newX[i][0], newX[i][1], newX[i][2], c="yellow")
     elif kmeans.labels_[i] == 3:
-        plt.scatter(newX[i][0], newX[i][1], c="green")
+        ax.scatter(newX[i][0], newX[i][1], newX[i][2], c="green")
     elif kmeans.labels_[i] == 4:
-        plt.scatter(newX[i][0], newX[i][1], c="purple")
+        ax.scatter(newX[i][0], newX[i][1], newX[i][2], c="purple")
     elif kmeans.labels_[i] == 5:
-        plt.scatter(newX[i][0], newX[i][1], c="black")
+        ax.scatter(newX[i][0], newX[i][1], newX[i][2], c="black")
 plt.show()
 
 data = pd.read_csv("final.csv")
